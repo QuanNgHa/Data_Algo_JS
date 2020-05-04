d2aeb10287ac18d531d6ccbbd2e8bf7673d00b6b
QuanNgHa <quanngha.dev@gmail.com>
2020-05-04 23:07:17 +0800

    Objects

--- Section2-DataStructures/array.js
+++ Section2-DataStructures/array.js
@@ -13,7 +13,8 @@ strings.pop();
 //         0    1    2    3    4
 strings.unshift('x');
 
-//splice
+//splice O(n): array.splice(start, deleteCount, items)
+// splice(2,0,'alien') means  at index 2, deleteCount = 0 => add, item = 'alien'
 strings.splice(2, 0, 'alien')
 
 console.log(strings);
