98e892b235845e209bb57a314951359a88c51c60
QuanNgHa <quanngha.dev@gmail.com>
2020-05-04 22:17:14 +0800

    Array

---
+++ Section2-DataStructures/array.js
@@ -0,0 +1,21 @@
+const strings = ['a', 'b', 'c', 'd']; //4*4 = 16 bytes of storage
+//Access O(1)
+console.log(strings[2]);
+
+//Push O(1)
+strings.push('e');
+
+//Pop O(1)
+strings.pop();
+
+//Unshift = Add 'x' in the begining of the Array
+// O(n): ['x', 'a', 'b', 'c', 'd']
+//         0    1    2    3    4
+strings.unshift('x');
+
+//splice
+strings.splice(2, 0, 'alien')
+
+console.log(strings);
+
+const numbers = [1, 2, 3, 4, 5];
