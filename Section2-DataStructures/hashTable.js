5fdcb48d13ae21c0421acb07f34238d8f640b010
CodeXplore <quanngha.dev@gmail.com>
2020-05-13 23:55:26 +0800

    Linked List Class

--- Section2-DataStructures/hashTable.js
+++ Section2-DataStructures/hashTable.js
@@ -12,4 +12,27 @@ user.spell = 'Added new key-value pair'; //Add O(1)
 console.log(user);
 
 
-const hashMap = new map()
+
+/* Hash Table using Map()*/
+var myMap = new Map();
+
+var keyString = 'a string',
+    keyObj = {},
+    keyFunc = function () { };
+
+// đặt các values
+myMap.set(keyString, "value associated with 'a string'");
+myMap.set(keyObj, 'value associated with keyObj');
+myMap.set(keyFunc, 'value associated with keyFunc');
+
+myMap.size; // 3
+
+// lấy các values
+myMap.get(keyString);    // "value associated with 'a string'"
+myMap.get(keyObj);       // "value associated with keyObj"
+myMap.get(keyFunc);      // "value associated with keyFunc"
+
+myMap.get('a string');   // "value associated with 'a string'"
+// ?vì keyString === 'a string'
+myMap.get({});           // undefined, vì keyObj !== {}
+myMap.get(function () { }) // undefined, vì  keyFunc !== function () {}
